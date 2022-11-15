bf8b497b2ffec8a41272f537a6ac805780448d4f
AnhTuyet Vo <voanht@flip1.engr.oregonstate.edu>
2022-11-14 20:30:52 -0800

    Fixed search function to display species name

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var express = require('express');   // We are using the express library for the 
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-PORT        = 9865;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 9866;                 // Set a port number at the top so it's easy to change in the future
 var db = require('./database/db-connector')
 
 const { engine } = require('express-handlebars');
@@ -37,21 +37,38 @@ app.get('/animals', function(req, res)
 
     // If there is no query string, we just perform a basic SELECT
     if (req.query.name === undefined){
-        query1 = "SELECT Animals.animal_id, Species.species_name, Animals.name, Animals.is_sick FROM Animals JOIN Species ON Animals.species_id = Species.species_id;";
+        query1 = `SELECT Animals.animal_id, Animals.name, Species.species_id, Animals.is_sick FROM Animals 
+        JOIN Species ON Animals.species_id = Species.species_id
+        GROUP BY Animals.animal_id ASC;`;
     }
     // If there is a query string, we assume this is a search, and return desired results
     else
     {
-        query1 = `SELECT * FROM Animals WHERE lower(Animals.name) LIKE "${req.query.name}%"`
+        query1 = `SELECT * FROM Animals WHERE lower(Animals.name) LIKE "%${req.query.name}%"`
     }
     
     let query2 = "SELECT * FROM Species;";
 
+    // Using an array to overwrite species_id with species_name
     db.pool.query(query1, function(error, rows, fields){
         let animals = rows;
 
         db.pool.query(query2, (error, rows, fields) => {
+
             let species = rows;
+
+            let speciesmap = {}
+                species.map(Species => {
+                    let id = parseInt(Species.species_id, 10);
+
+                    speciesmap[id] = Species["species_name"];
+                })
+
+                animals = animals.map(Animal => {
+                    return Object.assign(Animal, {species_id: speciesmap[Animal.species_id]})
+                })
+
+
             return res.render('animals', {data: animals, species: species});
         })
     })
@@ -131,8 +148,8 @@ app.get('/zookeepers', function(req, res)
 });
 
 // Add Animals
-app.post('/add-animal-ajax', function(req, res) {
-    let data = req.body
+app.post('/add-animal-ajax/', function(req, res) {
+    let data = req.body;
 
     query1 = `INSERT INTO Animals(name, species_id, is_sick)
     VALUES ('${data.name}', '${data.species_id}', '${data.is_sick}')`;
